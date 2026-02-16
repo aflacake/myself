fbce95ed584714e4635f1bdd3145fb6fd1371c42
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2026-02-16 16:44:01 +0700

    Play Traditional and Play Digital Pages
    Halaman utama (halaman arahan Play Page) dengan subhalaman konten Tradisional dan Digital.

---
+++ js/nav.js
@@ -0,0 +1,10 @@
+// js/nav.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.querySelector(".nav-toggle");
+  const menu = document.querySelector(".nav-menu");
+
+  toggle.addEventListener("click", () => {
+    menu.classList.toggle("open");
+  });
+});
